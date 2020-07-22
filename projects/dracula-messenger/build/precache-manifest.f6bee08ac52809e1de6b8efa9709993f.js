30036e5510d76b6d54d13252d38c17443d0121ad
Sasha Zakharchuk <petezzza@gmail.com>
2020-07-22 15:46:59 +0300

    Add Dracula Messenger

---
+++ projects/dracula-messenger/build/precache-manifest.f6bee08ac52809e1de6b8efa9709993f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb412d29be6659eb1f1af5db4fbe04af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed7bccb818184db731f",
+    "url": "/static/css/main.dbf5d755.chunk.css"
+  },
+  {
+    "revision": "50cce14e786e838e746c",
+    "url": "/static/js/2.f02b390a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f02b390a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed7bccb818184db731f",
+    "url": "/static/js/main.7c0452de.chunk.js"
+  },
+  {
+    "revision": "0d4be17ac67ed21f8520",
+    "url": "/static/js/runtime-main.ca12b74c.js"
+  }
+]);
